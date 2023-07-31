4354713a658440d1d54b19f9eb455d8b07b99a59
Kriti Singh <90671322+Kriti0802@users.noreply.github.com>
2023-07-31 12:39:49 +0530

    first commit

---
+++ frontend/src/components/People.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { getUsers } from '../getUsers';
+import axios from "axios";
+import "./css/People.css"
+function People() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const userFetch = () => {
+      axios.get("http://localhost:5000/api/userinfos").then((res) => {
+        console.log(res.data.data);
+        setUsers(res.data.data);
+      });
+    };
+
+    userFetch();
+  }, []);
+
+  return ( 
+    <div className='main-container'>
+      <h2 className='title'>Users</h2>
+      <div className='cards'>
+      {users.map((user) => (
+       
+        <div key={user._id} className='card'>
+          <div className='prof-image'>
+          <img src={user.photoURL} alt={"Not available"} />
+          </div>
+          <div className='username'>
+          <p>{user.name}</p>
+          </div>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
+
+export default People;
